6c9e8eb56552acd28f95f7ca9315acc092a99d96
DianaKalU <udiane81@gmail.com>
2023-07-18 17:22:55 -0700

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
-
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0
  */
-	int main(void)
-	{
-	     times_table();
-	     return (0);
-	}
+int main(void)
+{
+	times_table();
+	return (0);
+}
 
