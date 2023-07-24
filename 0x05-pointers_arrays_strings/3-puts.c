21bfc1251145d53ac84c0718f1c601dcc74069c5
DianaKalU <udiane81@gmail.com>
2023-07-24 15:17:57 -0700

    pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,13 @@
-include "main.h"
-/**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
- */
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
-		_putchar('\n');
-}
+ #include <unistd.h>
 
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, returns the character written as an unsigned char cast to an int.
+ *         On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c)
+{
+  return write(1, &c, 1);
+}
